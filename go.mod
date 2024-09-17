eed0adc6ee5199f1857bcc1458a51534dd6ad7d7
diegodevtech <diegodevtechnology@gmail.com>
2024-09-17 10:58:49 -0400

    feat(db): starts and finishes db config with /users method

--- go.mod
+++ go.mod
@@ -5,5 +5,8 @@ go 1.22.2
 require (
 	github.com/go-chi/chi v1.5.5
 	github.com/go-chi/cors v1.2.1
+	github.com/google/uuid v1.6.0
 	github.com/joho/godotenv v1.5.1
 )
+
+require github.com/lib/pq v1.10.9
---
+++ handler_user.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/diegodevtech/rss-aggregator-feed/internal/database"
+	"github.com/google/uuid"
+)
+
+func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request){
+
+	type parameters struct {
+		Name string `json:"name"`
+	}
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		RespondWithError(w, 400, fmt.Sprintf("Error parsing JSON: %v", err))
+		return
+	}
+	user, err := apiCfg.DB.CreateUser(r.Context(), database.CreateUserParams{
+		ID: uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name: params.Name,
+	})
+	if err != nil {
+		RespondWithError(w, 400, fmt.Sprintf("Couldn't create user: %v", err))
+		return
+	}
+
+	respondWithJSON(w, 200, databaseUserToUser(user))
+}
