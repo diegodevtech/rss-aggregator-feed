15dd50797e22dbf4d42d7ddac67ffb98997d92d6
diegodevtech <diegodevtechnology@gmail.com>
2024-10-02 15:02:20 -0400

    feat(many): implements logics from catching url to saving posts into the database

--- handler_user.go
+++ handler_user.go
@@ -39,3 +39,15 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request, user database.User){
 	respondWithJSON(w, 200, databaseUserToUser(user))
 }
+
+func (apiCfg *apiConfig) handlerGetPostsForUser(w http.ResponseWriter, r *http.Request, user database.User){
+	posts, err := apiCfg.DB.GetPostsForUser(r.Context(), database.GetPostsForUserParams{
+		UserID: user.ID,
+		Limit: 10,
+	})
+	if err != nil {
+		RespondWithError(w, 400, fmt.Sprintf("Couldn't get posts: %v", err))
+		return
+	}
+	respondWithJSON(w, 200, databasePostsToPosts(posts))
+}
---
+++ scraper.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/diegodevtech/rss-aggregator-feed/internal/database"
+	"github.com/google/uuid"
+)
+
+func startScraping(
+	db *database.Queries,
+	concurrency int,
+	timeBetweenRequest time.Duration,
+) {
+	log.Printf("Scraping on %v goroutines every %s duration", concurrency, timeBetweenRequest)
+	ticker := time.NewTicker(timeBetweenRequest)
+	for ; ; <-ticker.C {
+		feeds, err := db.GetNextFeedsToFetch(
+			context.Background(), int32(concurrency),
+		)
+		if err != nil {
+			log.Println("Error fetching feeds: ", err)
+			continue
+		}
+
+		wg := &sync.WaitGroup{}
+		for _, feed := range feeds {
+			wg.Add(1)
+			go scrapeFeed(db, wg, feed)
+		}
+		wg.Wait()
+	}
+}
+
+func scrapeFeed(db *database.Queries, wg *sync.WaitGroup, feed database.Feed) {
+	defer wg.Done()
+
+	_, err := db.MarkFeedAsFetched(context.Background(), feed.ID)
+	if err != nil {
+		log.Println("Error making feed as fetched: ", err)
+		return
+	}
+	rssFeed, err := urlToFeed(feed.Url)
+	if err != nil {
+		log.Println("Error fetching feed: ", err)
+		return
+	}
+	for _, item := range rssFeed.Channel.Item {
+		// log.Printf("Found post on [%s]: %s", feed.Name, item.Title)
+		description := sql.NullString{}
+		if item.Description != "" {
+			description.String = item.Description
+			description.Valid = true
+		}
+
+		publishedAt, err := time.Parse(time.RFC1123, item.PubDate)
+		if err != nil {
+			log.Printf("Couldn't parse date %v with err %v", item.PubDate, err)
+			continue
+		}
+		_, err = db.CreatePost(context.Background(),
+			database.CreatePostParams{
+				ID:          uuid.New(),
+				CreatedAt:   time.Now().UTC(),
+				UpdatedAt:   time.Now().UTC(),
+				Title:       item.Title,
+				Description: description,
+				PublishedAt: publishedAt,
+				Url:         item.Link,
+				FeedID:      feed.ID,
+			},
+		)
+		if err != nil {
+			if strings.Contains(err.Error(), "duplicate key"){
+				continue
+			}
+			fmt.Println("Failed to create post", err)
+		}
+
+	}
+	log.Printf("Feed %s collected, %v posts found", feed.Name, len(rssFeed.Channel.Item))
+}
